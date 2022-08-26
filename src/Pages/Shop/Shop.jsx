f0b5f8fa20ba107c9a63269011f0ffb85b4e4eaf
AvetBadalyan <avetbadalyan@gmail.com>
2022-08-26 15:41:44 +0400

    feat: map mock data to shop

--- src/Pages/Shop/Shop.jsx
+++ src/Pages/Shop/Shop.jsx
@@ -1,5 +1,10 @@
 import React from "react";
+import SHOP_DATA from "./../../shop-data.json";
 
 export default function Shop() {
-  return <div>Shop</div>;
+  return <div>
+    {SHOP_DATA.map(product => <div key={product.id}>
+      <h1>{product.name}</h1>
+    </div> )}
+  </div>;
 }
